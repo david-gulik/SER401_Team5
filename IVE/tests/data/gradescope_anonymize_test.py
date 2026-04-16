31913e1fa8b010d14f6ae47bf082af1839764794
David Gulik <dgulik@asu.edu>
2026-04-16 16:39:39 -0500

    Ruff formatted test file

--- IVE/tests/data/gradescope_anonymize_test.py
+++ IVE/tests/data/gradescope_anonymize_test.py
@@ -1,43 +1,45 @@
-import pytest
 import preparation
-import zipfile
-
-from preparation import anonymize_gradescope_submissions
 
 
 def test_simple_line_comment():
-    expected = 'int x = 1; \nint y = 2;\n'
-    src = 'int x = 1; // set x\nint y = 2;\n'
+    expected = "int x = 1; \nint y = 2;\n"
+    src = "int x = 1; // set x\nint y = 2;\n"
     result = preparation.strip_gradescope_comments(src)
     assert result == expected
 
+
 def test_no_comment():
-    src = 'int x = 1; \nint y = 2;\n'
+    src = "int x = 1; \nint y = 2;\n"
     result = preparation.strip_gradescope_comments(src)
     assert result == src
 
+
 def test_single_line_block_comment():
-    src = 'int x = 1; /* block comment */ int y = 2;\n'
-    expected = 'int x = 1;  int y = 2;\n'
+    src = "int x = 1; /* block comment */ int y = 2;\n"
+    expected = "int x = 1;  int y = 2;\n"
     result = preparation.strip_gradescope_comments(src)
     assert result == expected
 
+
 def test_multi_line_block_comment():
-    src = 'int x = 1; /* block comment \n \n with several lines */ int y = 2;\n'
-    expected = 'int x = 1;  int y = 2;\n'
+    src = "int x = 1; /* block comment \n \n with several lines */ int y = 2;\n"
+    expected = "int x = 1;  int y = 2;\n"
     result = preparation.strip_gradescope_comments(src)
     assert result == expected
+
 
 def test_string_comment():
     src = 'String x = "//not a comment"'
     result = preparation.strip_gradescope_comments(src)
     assert result == src
 
+
 def test_comment_markers_inside_char_literal():
     src = "char c = '/'; char d = '*'; // this is a real comment\n"
     expected = "char c = '/'; char d = '*'; \n"
     result = preparation.strip_gradescope_comments(src)
     assert result == expected
+
 
 def test_sloppy_comment_markers():
     src = "int x = 1;/////////// comment\nint y = 2;/*/*/\nint z = 3;//comment\n"
