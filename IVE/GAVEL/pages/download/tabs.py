2693b2981a0f3be7c71bed858e2e196ec2d2abe1
baileybourque <baileybourque97@gmail.com>
2026-09-12 14:50:49 -0600

    Fix merge conflicts

--- IVE/GAVEL/pages/download/tabs.py
+++ IVE/GAVEL/pages/download/tabs.py
@@ -3,7 +3,7 @@ from __future__ import annotations
 import os
 from pathlib import Path
 
-from PyQt6.QtCore import QTimer
+from PyQt6.QtCore import Qt, QTimer
 from PyQt6.QtWidgets import (
     QComboBox,
     QFileDialog,
