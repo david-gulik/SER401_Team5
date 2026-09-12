440b55133340f95af8ff9ec82e23167ec7659d38
Carli VonWeinstein <cvonwein@asu.edu>
2026-09-12 17:16:31 -0400

    address logger propagation review feedback

--- IVE/GAVEL/services/logger.py
+++ IVE/GAVEL/services/logger.py
@@ -6,8 +6,10 @@ import logging
 class AppLogger:
     def __init__(self, name: str = "GAVEL", propagate: bool = True) -> None:
         self._logger = logging.getLogger(name)
-        self._logger.propagate = propagate
+
         if not self._logger.handlers:
+            self._logger.propagate = propagate
+
             handler = logging.StreamHandler()
             fmt = logging.Formatter("[%(levelname)s] %(name)s: %(message)s")
             handler.setFormatter(fmt)
