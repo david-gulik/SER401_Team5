e314a82ad5af217ce8b07c4295527eff75db3dc1
David Gulik <dgulik@asu.edu>
2026-09-26 13:13:33 -0500

    Removed unused import

--- IVE/tests/infra/gradescope/test_gradescope.py
+++ IVE/tests/infra/gradescope/test_gradescope.py
@@ -1,4 +1,4 @@
-from unittest.mock import MagicMock, patch
+from unittest.mock import MagicMock#, #patch
 
 from GAVEL.infra.gradescope.http_gradescope_client import GradescopeSession, http_gradescope_client
 
